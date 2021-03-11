813b7877df3d22cb256ff243b951221ff32c5b48
Remco Haszing <remcohaszing@gmail.com>
2021-03-11 22:26:24 +0100

    Generate type definitions from jsdoc

--- index.js
+++ index.js
@@ -6,17 +6,32 @@ exports.name = name
 
 var id = require('./regex')
 
+/**
+ * Checks if the given character code can start an identifier.
+ *
+ * @param {number} code
+ */
 // To do: support astrals.
 function start(code) {
   return id.start.test(String.fromCharCode(code))
 }
 
+/**
+ * Checks if the given character code can continue an identifier.
+ *
+ * @param {number} code
+ */
 // To do: support astrals.
 function cont(code) {
   var character = String.fromCharCode(code)
   return id.start.test(character) || id.cont.test(character)
 }
 
+/**
+ * Checks if the given string is a valid identifier name.
+ *
+ * @param {string} name
+ */
 function name(name) {
   var index = -1
 
